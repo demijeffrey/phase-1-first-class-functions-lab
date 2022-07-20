9e33d5be692d8b36c4da9b12ad2742a66e65d24b
Demaris Ashmore <demiashmore@yahoo.com>
2022-07-19 22:26:27 -0600

    commit tree 6bd129f7f84cd29322ac6d947462b8fe1bf16cd9

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(drivers) {
+    const copyOfDrivers = drivers.slice(0,2)
+    return copyOfDrivers
+}
+
+const returnLastTwoDrivers = function() {
+    const copyOfDrivers = drivers.slice(2)
+    return copyOfDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (distance) {
+    return function(fare) {
+        return fare * distance
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, functions) => {
+    return functions(drivers)
+}
